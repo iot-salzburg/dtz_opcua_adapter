0f6b1a3df2a97470c1ae7ce7bc491fb76598fac9
ChristophSchranz <christoph.schranz@salzburgresearch.at>
2018-09-14 11:12:55 +0200

    Made scripts executable, changed name, excluded port

--- show-adapter-stats.sh
+++ show-adapter-stats.sh
@@ -2,5 +2,5 @@
 echo "Printing 'docker service ls | grep add-opcua':"
 docker service ls | grep add-opcua
 echo ""
-echo "Printing 'docker service ps add-opcua':"
-docker service ps add-opcua
+echo "Printing 'docker service ps add-opcua_adapter':"
+docker service ps add-opcua_adapter
--- stop-mqtt-adapter.sh
+++ stop-mqtt-adapter.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker stack rm add-opcua
+docker stack rm add-opcua_adapter
 echo "Wait some seconds before restarting the stack"
